b82fa45fd5d3afa5a2318d4ebe00218c4459d5a1
doppelpunkt3 <56027130+doppelpunkt3@users.noreply.github.com>
2019-11-05 19:08:27 +0100

    C#-Zusammenfassung erstellt und gefüllt

---
+++ C#-Zusammenfassung(-1.KA)/Taschenrechner (+-)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace __
+{
+    class Program
+    {
+        static void Main(string[] args)
+            Console.WriteLine("Möchten sie + oder - rechnen? (+/-)");
+            string antwort = Console.ReadLine();
+            //Wenn die Antwort "+" ist, wird folgendes ausgeführt:
+            if(antwort == "+")
+            {
+                Console.WriteLine("Geben sie nacheiner 2 Zahlen ein um diese miteinenader zu addieren:\n" +
+                    "1. Zahl:");
+                decimal zahl1 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("2. Zahl:");
+                decimal zahl2 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("\nDas Ergebnis beträgt");
+                Console.WriteLine(zahl1 + zahl2);
+                Console.ReadLine();
+            }
+            //Wenn die Antwort "-" ist, wird folgendes ausgeführt:
+            if(antwort == "-")
+            {
+                Console.WriteLine("Geben sie nacheinander 2 Zahlen ein um diese miteinenader zu subtrahieren:\n" +
+                    "1. Zahl:");
+                decimal zahl1 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("\n2. Zahl:");
+                decimal zahl2 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("\nDas Ergebnis beträgt");
+                Console.WriteLine(zahl1 - zahl2);
+                Console.ReadLine();
+            }
+            //Wenn keines der Antworten "+","-" beinhaltet, wird das Programm geschlossen:
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
