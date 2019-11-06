ec9e03f102a4613bb33801d0d23bd6abe1531f41
Dzefo <dzefo@outlook.de>
2019-11-06 08:39:06 +0100

    Kommentare & else if hinzugefügt

--- C#-Zusammenfassung(-1.KA)/Vergleichsoperatoren/Program.cs
+++ C#-Zusammenfassung(-1.KA)/Vergleichsoperatoren/Program.cs
@@ -1,51 +1,53 @@
-﻿using System;
+﻿//Author: Konrad
+using System;
 
 namespace Vergleichsoperatoren
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Dieses Programm ist eine Veranschaulichung der Vergleichoperatoren, die wir behandelt haben.\n");
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Dieses Programm ist eine Veranschaulichung der Vergleichoperatoren, die wir behandelt haben.\n");
 
-            Console.WriteLine("Bitte gib 2 Zahlen ein, die miteinander verglichen werden sollen:\n");
-            int zahl1 = Convert.ToInt32(Console.ReadLine());
-            int zahl2 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Bitte gib 2 Zahlen ein, die miteinander verglichen werden sollen:\n");
+			int zahl1 = Convert.ToInt32(Console.ReadLine());
+			int zahl2 = Convert.ToInt32(Console.ReadLine());
 
-            if (zahl1 == zahl2)
-            {
-                Console.WriteLine("Zahl1 = Zahl2");
-            }
-
-            if(zahl1 != zahl2)
-            {
-                Console.WriteLine("Zahl1 ist nicht gleich Zahl2");
-            }
-
-            if(zahl1 <= zahl2)
-            {
-                Console.WriteLine("Zahl1 ist größer oder gleich wie Zahl2");
-            }
-
-            if(zahl1 >= zahl2)
-            {
-                Console.WriteLine("Zahl1 ist kleiner oder gleich wie Zahl2");
-            }
-
-            if(zahl1 > zahl2)
-            {
-                Console.WriteLine("Zahl1 ist großer als Zahl2");
-            }
-
-            if(zahl1 < zahl2)
-            {
-                Console.WriteLine("Zahl1 ist kleiner als Zahl2");
-            }
-
-            else
-            {
-                Environment.Exit(0);
-            }
-        }
-    }
+			// Wenn gleich
+			if (zahl1 == zahl2)
+			{
+				Console.WriteLine("Zahl1 = Zahl2");
+			}
+			// Wenn nicht gleich
+			else if (zahl1 != zahl2)
+			{
+				Console.WriteLine("Zahl1 ist nicht gleich Zahl2");
+			}
+			// Wenn kleiner gleich
+			else if (zahl1 <= zahl2)
+			{
+				Console.WriteLine("Zahl1 ist größer oder gleich wie Zahl2");
+			}
+			// Wenn größer gleich
+			else if (zahl1 >= zahl2)
+			{
+				Console.WriteLine("Zahl1 ist kleiner oder gleich wie Zahl2");
+			}
+			// Wenn größer
+			else if (zahl1 > zahl2)
+			{
+				Console.WriteLine("Zahl1 ist großer als Zahl2");
+			}
+			// Wenn kleiner
+			else if (zahl1 < zahl2)
+			{
+				Console.WriteLine("Zahl1 ist kleiner als Zahl2");
+			}
+			// Wenn nichts von dem oberen (hier nicht möglich)
+			else
+			{
+				Environment.Exit(0);
+			}
+		}
+	}
 }
