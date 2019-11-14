13ceec2c71ba59831d0a64ed5e381de0e1df8692
doppelpunkt3 <56027130+doppelpunkt3@users.noreply.github.com>
2019-11-14 10:03:51 +0100

    +Comments

--- C#/Lernaufgabe(Team-2)/Lernaufgabe2 (Team)/Program.cs
+++ C#/Lernaufgabe(Team-2)/Lernaufgabe2 (Team)/Program.cs
@@ -10,29 +10,40 @@ namespace Lernaufgabe2__Team_
     {
         static void Main(string[] args)
         {
+            //Abfragen des Wertes, bei dem die nachfolgende for-Schleife anfangen soll
             Console.WriteLine("Ab wann: ");
             int abwann = Convert.ToInt32(Console.ReadLine());
 
+            //Abfragen des Wertes, bei dem die for-Schleife enden soll
             Console.WriteLine("Bis wann: ");
             int biswann = Convert.ToInt32(Console.ReadLine());
 
+            //Abfragen des Teilers, den die for-Schleife benutzen soll
             Console.WriteLine("Welchen Teiler?: ");
             int teiler = Convert.ToInt32(Console.ReadLine());
             
-
-            for(int i = abwann; i<= biswann; i++)
+            /*For-Schleife:
+            Variable i, Typ: Integer wird erstellt und auf den bereits abgefragten Wert (abwann) gesetzt
+            Solange i kleiner/gleich dem Wert (biswann) ist, wird folgendes ausgeführt
+            Nach jedem Durchgang wird i um 1 erhöht*/
+            for(int i = abwann; i <= biswann; i++)
             {
+                //Wenn i durch den bereits abgefragten Teiler teilbar ist ohne einen Rest zu hinterlassen
                 if(i % teiler == 0)
                 {
-                    Console.BackgroundColor = ConsoleColor.Red;
+                    //Schrift wird auf Rot gesetzt und die Zahlen werden mit dem Prefix "Eingefärbt" ausgegeben
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Eingefärbt: " + i);
                 }
+                //Wenn die Bedingung nicht erfüllt ist, wird folgendes ausgeführt
                 else
                 {
-                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    //Schrift wird auf Cyan gesetzt und die aktuelle Zahl wird ausgegeben
+                    Console.ForegroundColor = ConsoleColor.Cyan;
                     Console.WriteLine(i);
                 }
             }
+            //Console wird offengehalten
             Console.ReadLine();
         }
     }
--- C#/Schleifen Einführung/Schleifen Einführung/Program.cs
+++ C#/Schleifen Einführung/Schleifen Einführung/Program.cs
@@ -10,10 +10,15 @@ namespace Schleifen_Einführung
     {
         static void Main(string[] args)
         {
+            //Variable i, Typ: Integer wird erstellt und auf 0 gesetzt
+            //Solange i kleiner als 100 ist, wird folgendes ausgeführt
+            //Nach jedem Durchlauf wird i um 1 erhöht
             for(int i=0; i < 100; i++)
             {
+                //Ausgeben der aktuellen Zahl
                 Console.WriteLine("Aktuelle Zahl: " + i);
             }
+            //Console wird offengelassen
             Console.ReadKey();
         }
     }
