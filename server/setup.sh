a2c64c3544039b59a90cb7ef1d9f1590604465e7
Dzefo <dzefo@outlook.de>
2019-11-15 14:35:43 +0100

    shell Änderungen

--- server/setup.sh
+++ server/setup.sh
@@ -1,60 +1,30 @@
 if [ -f /etc/systemd/system/ki219.service ]
-
 then
-
 service ki219 stop
-
 rm /etc/systemd/system/ki219.service
-
 systemctl daemon-reload
-
 fi
 
-
-rm /home/ki219/start.sh
-
-
+rm $PWD/start.sh
 {
-
 echo '#!/bin/bash'
-
-echo 'cd /home/ki219'
-
+echo 'cd ${PWD}'
 echo 'npm start'
-
-}>/home/ki219/start.sh
-
-
-chmod +x /home/ki219/start.sh
-
+}>$PWD/start.sh
+chmod +x $PWD/start.sh
 
 {
-
 echo '[Unit]'
-
 echo 'Description=ki219'
-
 echo 'StartLimitIntervalSec=0'
-
 echo '[Service]'
-
 echo 'Type=simple'
-
 echo 'Restart=always'
-
 echo 'RestartSec=1'
-
 echo 'User=root'
-
-echo 'ExecStart=/home/ki219/start.sh'
-
+echo 'ExecStart=${PWD}/start.sh'
 echo '[Install]'
-
 echo 'WantedBy=multi-user.target '
-
 }>/etc/systemd/system/ki219.service
-
-
 systemctl enable ki219
-
 service ki219 start
--- server/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ki219
-npm start
